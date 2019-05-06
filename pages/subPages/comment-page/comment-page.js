0ef1554e05aad2d9c9b8d1653a405b0844d45867
jianfengtheboy <jianfengtheboy@163.com>
2019-05-06 17:11:36 +0800

    add comment-page

--- pages/subPages/comment-page/comment-page.js
+++ pages/subPages/comment-page/comment-page.js
@@ -1,66 +1,87 @@
 // pages/subPages/comment-page/comment-page.js
+const util = require("../../../utils/util.js")
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    movieId: '',
+    //最新评论
+    cmts: [],
+    //热门评论
+    hcmts: [],
+    //是否加载完成
+    loadComplete: false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    this.initPage(options)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //初始化
+  initPage(options) {
+    let movieId = options.movieId
+    let movieName = options.movieName
+    wx.setNavigationBarTitle({
+      title: `观众评论-${movieName}`
+    })
+    wx.showLoading({
+      title: '正在加载...'
+    })
+    this.getComment(movieId)
+    this.setData({
+      movieId
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onReachBottom() {
+    this.getComment(this.data.movieId)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //获取观众评论
+  getComment(movieId) {
+    if (this.data.loadComplete) {
+      return
+    }
+    let cmts = this.data.cmts
+    let _this = this
+    wx.request({
+      url: `https://m.maoyan.com/mmdb/comments/movie/${movieId}.json?_v_=yes&offset=${cmts.length}`,
+      success(res) {
+        let comments = { ...res.data }
+        const newCmts = cmts.concat(_this.formatData(comments.cmts))
+        wx.hideLoading()
+        _this.setData({
+          hcmts: _this.formatData(comments.hcmts),
+          cmts: newCmts,
+          loadComplete: newCmts.length >= comments.total
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //处理数据
+  formatData(arr) {
+    let list = []
+    if (arr.length) {
+      list = arr.map(item => {
+        let temp = { ...item }
+        temp.avatarurl = temp.avatarurl || '/assets/images/avatar.png'
+        temp.purchase = !!(temp.tagList && temp.tagList.fixed.some(item => item.id === 4))
+        temp.stars = this.formatStar(temp.score)
+        temp.calcTime = util.calcTime(temp.startTime)
+        return temp
+      })
+    }
+    return list
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //处理评分星星
+  formatStar(sc) {
+    //1分对应满星，0.5对应半星
+    let stars = new Array(5).fill('star-empty')
+    //满星的个数
+    const fullStars = Math.floor(sc)
+    //半星的个数，半星最多1个
+    const halfStar = sc % 1 ? 'star-half' : 'star-empty'
+    //填充满星
+    stars.fill('star-full', 0, fullStars)
+    if (fullStars < 5) {
+      //填充半星
+      stars[fullStars] = halfStar
+    }
+    return stars
   }
 })
