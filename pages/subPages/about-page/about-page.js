6ba32bf0a26e07a5dbaf0f6eac44ec1f2c7a582c
jianfengtheboy <jianfengtheboy@163.com>
2019-05-12 08:50:45 +0800

    add about-page && optimise CSS

--- pages/subPages/about-page/about-page.js
+++ pages/subPages/about-page/about-page.js
@@ -1,66 +1,27 @@
 // pages/subPages/about-page/about-page.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    info: [
+      {
+        label: '姓名',
+        value: 'Sun'
+      },
+      {
+        label: '职业',
+        value: '程序猿/攻城狮'
+      },
+      {
+        label: 'QQ',
+        value: '1690355023'
+      },
+      {
+        label: 'Wechat',
+        value: 'jianfengtheboy'
+      },
+      {
+        label: 'github',
+        value: 'https://github.com/jianfengtheboy'
+      }
+    ]
   }
 })
